2c70a20c133360bddaaae8a00ad6deb6fe446ea2
Ilja Lukin <ilja.lukin@fh-dortmund.de>
2020-12-22 14:05:45 +0100

    update composer dependencies

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-         Commands\CleanupTranscode::class
+         Commands\CleanupTranscode::class,
+         Commands\Version::class
     ];
 
     /**
---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v1.0.1-pre',
+  'full_app_version' => 'v1.0.1-pre - build 36-g958c4a18',
+  'build_version' => '36',
+  'prerelease_version' => '',
+  'hash_version' => 'g958c4a18',
+  'full_hash' => 'v1.0.0-1-g958c4a18',
+  'branch' => 'dev',
+);
